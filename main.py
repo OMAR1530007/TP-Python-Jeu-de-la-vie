941d5b535a3ecbf8b9e7bb412e13c2399293e0e7
benphihe <150243312+benphihe@users.noreply.github.com>
2024-11-19 13:59:39 +0100

    creation d'une grille avec 0 et 1 aléatoire

---
+++ main.py
@@ -0,0 +1,15 @@
+import random
+
+def creer_grille(n, m):
+    grille = []
+    for _ in range(n):
+        ligne = [random.randint(0, 1) for _ in range(m)]
+        grille.append(ligne)
+    return grille
+
+
+n = 5
+m = 5
+grille = creer_grille(n, m)
+for ligne in grille:
+    print(ligne)
